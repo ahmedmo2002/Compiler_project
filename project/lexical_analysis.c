df4e37287f9d64143503c120b551cb45c44dc514
ahmedmo2002 <117599120+ahmedmo2002@users.noreply.github.com>
2023-12-30 17:36:12 +0200

    Add files via upload

---
+++ project/lexical_analysis.c
@@ -0,0 +1,87 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_TOKEN_LENGTH 100
+
+typedef enum {
+    KEYWORD,
+    IDENTIFIER,
+    OPERATOR,
+    Numeric_const,
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char lexeme[MAX_TOKEN_LENGTH];
+} Token;
+
+char* keywords[] = {"int", "float", "char", "if", "else", "while", "for"};
+char* OPERATORS[] = {"+", "-", "*", "/", "="};
+
+int isKeyword(char *word) {
+    for (int i = 0; i < sizeof(keywords) / sizeof(keywords[0]); i++) {
+        if (strcmp(keywords[i], word) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+int isOperator(char *operat) {
+    for (int i = 0; i < sizeof(OPERATORS) / sizeof(OPERATORS[0]); i++) {
+        if (strcmp(OPERATORS[i], operat) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+Token* lexicalAnalysis(char *input) {
+    Token *tokens = malloc(sizeof(Token) * MAX_TOKEN_LENGTH);
+    int tokenCount = 0;
+    char *token = strtok(input, " ");
+    while (token != NULL) {
+        Token newToken;
+        if (isKeyword(token)) {
+            newToken.type = KEYWORD;
+        }
+        else if(isOperator(token)){
+                newToken.type = OPERATOR;
+        }
+        else if (isdigit(token[0])) {
+            newToken.type = Numeric_const;
+        } else {
+            newToken.type = IDENTIFIER;
+        }
+        strcpy(newToken.lexeme, token);
+        tokens[tokenCount] = newToken;
+        tokenCount++;
+        token = strtok(NULL, " ");
+    }
+    tokens[tokenCount].type = -1;
+    return tokens;
+}
+
+int main() {
+    char input[] = "int num1 = 7; float num2 = 17; float num3 = num1 + num2;";
+    Token *tokens = lexicalAnalysis(input);
+    int i = 0;
+    while (tokens[i].type != -1) {
+        if (tokens[i].type == KEYWORD) {
+            printf("Keyword: %s\n", tokens[i].lexeme);
+        } else if (tokens[i].type == IDENTIFIER) {
+            printf("Identifier: %s\n", tokens[i].lexeme);
+        }
+          else if (tokens[i].type == OPERATOR) {
+            printf("Operator: %s\n", tokens[i].lexeme);
+        } else if (tokens[i].type == Numeric_const) {
+            printf("Numeric Const: %s\n", tokens[i].lexeme);
+        }
+        i++;
+    }
+    return 0;
+}
+
+*/
---
+++ project/syntax_analysis.c
@@ -0,0 +1,73 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+    KEYWORD,
+    IDENTIFIER,
+    OPERATOR,
+    Numeric_const,
+    SPECIALCHAR
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char lexeme[100];
+} Token;
+
+int syntaxAnalysis(Token *tokens) {
+    int i = 0;
+    while (tokens[i].type != -1) {
+        if (tokens[i].type == KEYWORD) {
+            i++;
+            if (tokens[i].type == IDENTIFIER) {
+                i++;
+                if (strcmp(tokens[i].lexeme, "=") == 0) {
+                    i++;
+                    if (tokens[i].type == Numeric_const) {
+                        i++;
+                        if (strcmp(tokens[i].lexeme, ";") == 0) {
+                            i++;
+                        } else {
+                            printf("Syntax error: Missing semicolon.\n");
+                            return 0;
+                        }
+                    } else {
+                        printf("Syntax error: Expected literal after assignment operator.\n");
+                        return 0;
+                    }
+                } else {
+                    printf("Syntax error: Expected assignment operator.\n");
+                    return 0;
+                }
+            } else {
+                printf("Syntax error: Expected identifier after keyword.\n");
+                return 0;
+            }
+        } else {
+            printf("Syntax error: Unexpected token.\n");
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+
+    Token tokens[] = {
+        {KEYWORD, "int"},
+        {IDENTIFIER, "x"},
+        {OPERATOR, "="},
+        {Numeric_const, "10"},
+        {-1, ""}
+    };
+
+    if (syntaxAnalysis(tokens)) {
+        printf("Syntax analysis succeeded.\n");
+    } else {
+        printf("Syntax analysis failed.\n");
+    }
+
+    return 0;
+}
